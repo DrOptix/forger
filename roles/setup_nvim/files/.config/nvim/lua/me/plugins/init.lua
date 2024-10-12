ad1e8e1e431be5d3639ab97e4c43df6b90d95f97
Cristian Dinu <cristian.virgil.dinu@gmail.com>
2024-10-12 07:40:23 +0300

    feat(roles.setup_nvim): Add `lazy.nvim` manager
    - Install `git`, it's a dependency of `lazy.nvim`
    - Include `vim-tmux-navigator` plugin
    - Automatically install plugins by running `lazy.nvim` in a
      headless `nvim`.

---
+++ roles/setup_nvim/files/.config/nvim/lua/me/plugins/init.lua
@@ -0,0 +1,7 @@
+-- Put plugins that require no configuration here, otherwise create
+-- a separate `plugin.lua` file in `./lua/me/plugins`.
+
+return {
+    -- tmux & split window navigation
+    "christoomey/vim-tmux-navigator",
+}
