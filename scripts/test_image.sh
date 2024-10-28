d0f6303361155fbc4385b1fd7dc6603fb9190c31
Cristian Dinu <cristian.virgil.dinu@gmail.com>
2024-10-28 22:48:45 +0200

    refactor(scripts): Remove progress and verbosity
    The script is used for development, better see clearly if something
    blows up.

--- scripts/test_image.sh
+++ scripts/test_image.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Define colors for output
-GREEN="\033[0;32m"
-RED="\033[0;31m"
-NC="\033[0m" # No Color
-
 # Get the directory of the current script
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
@@ -20,7 +15,7 @@ done
 
 # Check if the first argument is provided
 if [ -z "$1" ]; then
-    echo "Usage: $0 [-v] <os_type>"
+    echo "Usage: $0 <os_type>"
     echo "Available OS types:"
     for os_type in "${OS_TYPES[@]}"; do
         echo " - $os_type"
@@ -32,20 +27,6 @@ fi
 VERBOSE=false
 IMAGE_TAG=""
 
-# Parse command-line arguments
-while [[ $# -gt 0 ]]; do
-    case $1 in
-        -v)
-            VERBOSE=true
-            shift
-            ;;
-        *)
-            IMAGE_TAG="$1"
-            shift
-            ;;
-    esac
-done
-
 # Check if the provided OS type is valid
 VALID_OS=false
 for os_type in "${OS_TYPES[@]}"; do
@@ -55,22 +36,6 @@ for os_type in "${OS_TYPES[@]}"; do
     fi
 done
 
-# Function to show a spinner
-show_spinner() {
-    local pid=$1
-    local delay=0.1
-    local spinstr="|/-\\"
-    local msg=$2
-    printf "%s" "$msg"
-    while kill -0 "$pid" 2>/dev/null; do
-        for (( i=0; i<${#spinstr}; i++ )); do
-            printf "\r%s [%c] " "$msg" "${spinstr:$i:1}"
-            sleep "$delay"
-        done
-    done
-    printf "\r%s    " "$msg" # Clear spinner
-}
-
 # If the OS type is valid, build and run the image using Podman
 if $VALID_OS; then
     dockerfile="$DOCKERFILES_DIR/Dockerfile.$IMAGE_TAG"
@@ -78,19 +43,11 @@ if $VALID_OS; then
     # Message to display while building
     msg="Building image forger_test:${IMAGE_TAG}:"
 
-    if $VERBOSE; then
-        podman build -t "forger_test:$IMAGE_TAG" -f "$dockerfile" "$DOCKERFILES_DIR/.."
-    else
-        podman build -t "forger_test:$IMAGE_TAG" -f "$dockerfile" "$DOCKERFILES_DIR/.." > /dev/null 2>&1 &
-        pid=$!
-        show_spinner "$pid" "$msg"
-        wait "$pid"
-    fi
+    podman build -t "forger_test:$IMAGE_TAG" -f "$dockerfile" "$DOCKERFILES_DIR/.."
 
     build_status=$?
 
     if [[ $build_status -eq 0 ]]; then
-        echo -e "\r${msg} ${GREEN}OK${NC}"
         echo "Running image forger_test:$IMAGE_TAG"
 
         # Allow containers to connect to host X11 display
@@ -113,12 +70,12 @@ if $VALID_OS; then
         # Remove container access to host X11 display
 	    xhost -local:docker > /dev/null 2>&1
     else
-        echo -e "\r${msg} ${RED}FAIL${NC}"
+        echo "Failed to build image forger_test:$IMAGE_TAG"
         exit 1
     fi
 else
     echo "Invalid OS type: $IMAGE_TAG"
-    echo "Usage: $0 [-v] <os_type>"
+    echo "Usage: $0 <os_type>"
     echo "Available OS types:"
     for os_type in "${OS_TYPES[@]}"; do
         echo " - $os_type"
