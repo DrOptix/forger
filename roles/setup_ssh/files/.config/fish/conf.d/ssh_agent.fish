de9f32f5b9817a6162ed0a8d166938420778299a
Cristian Dinu <cristian.virgil.dinu@gmail.com>
2024-10-29 22:22:01 +0200

    fix: Always export `ssh-agent` variables
    We always create the same `~/.ssh/ssh-agent.socket` socket when we start
    `ssh-agent` because of this we can always prepare the correct value for
    `SSH_AUTH_SOCK`. For `SSH_AGENT_PID` we just use `pgrep` to find the PID
    of `ssh-agent`.
    
    This helps because even after a fresh run of the playbook when `fish`
    starts it will have the `ssh-agent` variables set, even `ssh-agent` was
    started by the playbook.
    
    NOTE: The only thing to note is that the playbook needs to create the
    same `~/.ssh/ssh-agent.socket` socket when starting `ssh-agent`.k

--- roles/setup_ssh/files/.config/fish/conf.d/ssh_agent.fish
+++ roles/setup_ssh/files/.config/fish/conf.d/ssh_agent.fish
@@ -1,9 +1,11 @@
 #!/usr/bin/env fish
 
-if not pgrep -u $USER ssh-agent > /dev/null
-    eval (ssh-agent -c -a /home/$USER/.ssh/ssh_agent.socket)
+set socket_path "/home/$USER/.ssh/ssh_agent.socket"
 
-    set -gx SSH_AUTH_SOCK "$SSH_AUTH_SOCK"
-    set -gx SSH_AGENT_PID "$SSH_AGENT_PID"
+if not pgrep -u $USER ssh-agent > /dev/null
+    eval (ssh-agent -c -a "$socket_path")
 end
+
+set -gx SSH_AUTH_SOCK "$socket_path"
+set -gx SSH_AGENT_PID "$(pgrep ssh-agent)"
 
