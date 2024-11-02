ac2888ba815ac21f638d327eee6c448a975b7ef6
Cristian Dinu <cristian.virgil.dinu@gmail.com>
2024-11-02 20:35:09 +0200

    feat(setup_shell): Load `environment` file for `fish`
    This is the second part of `environment` loading. As the title suggests
    we do it for `fish` this time.
    
    The behavior is the same as for `bash`.
    
    Now we can have the same environment for both `fish` and `bash`.
    
    I've decided to do this because `fish` is not POSIX compliant. It's
    scripting is nicer, but until I'm sure I can go full `fish` with this
    one I'll do the best of both worlds: `fish` in terminal and `bash` in
    services and other automation stuff.

--- roles/setup_shell/files/.config/fish/config.fish
+++ roles/setup_shell/files/.config/fish/config.fish
@@ -2,9 +2,42 @@ abbr -a l 'ls'
 abbr -a ll 'ls -lh'
 abbr -a lla 'ls -lah'
 
-# In most cases ~/.local/bin is not added to $PATH
-if not echo $PATH | grep -q ~/.local/bin
-    set -gx PATH ~/.local/bin $PATH
+# NOTE: No need to load system wide config as they are loaded
+#       by default.
+
+set ENV_FILE "$HOME/.config/environment"
+
+if test -f $ENV_FILE
+    for line in (cat "$ENV_FILE")
+        # Parse and process ADD_PATH lines, ignoring comments and empty lines
+        if not string match -qr "^\s*#" "$line" && string match -qr "^\s*ADD_PATH" "$line"
+            # Extract the path after "ADD_PATH"
+            set path_to_add (string replace "ADD_PATH " "" "$line")
+            set path_to_add (string replace -a '"' " " "$path_to_add")
+            set path_to_add (string trim "$path_to_add")
+
+            # Add to PATH if not already present
+            if not string match -r ":$PATH:" -- ":$path_to_add:"
+                set -gx PATH "$path_to_add:$PATH"
+            end
+        end
+
+        # Parse and process VAR lines, ignoring commends and empty lines
+        if not string match -qr "^\s*#" "$line" && string match -qr "^\s*VAR" "$line"
+            # Extract variable name and value
+            set line (string replace "VAR " "" "$line")
+            set line (string trim "$line" )
+            set line (string split " " "$line")
+            set var_name $line[1]
+            set var_value (string join " " (string split " " $line)[2..])
+
+            # Remove quotes if they exist
+            set var_value (string replace -a '"' "" "$var_value")
+
+            # Export the variable
+            set -gx $var_name $var_value
+        end
+    end
 end
 
 function fish_prompt
